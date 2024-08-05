e6bee55d67aca96a5e81e83a1f9e2d15a7814ea8
HAJIRA-NASEEM <hajiranaseem1@outlook.com>
2024-08-06 03:56:22 +0500

    initialize my repo

---
+++ main.py
@@ -0,0 +1,11 @@
+num1=float(input("Enter the no 1: "))
+num2=float(input("Enter the no 2: "))
+operator=input("Enter the operator: ")
+if operator=="+":
+    print(num1+num2)
+elif operator=="-":
+    print(num1-num2)
+elif operator=="*":
+    print(num1*num2)
+elif operator=="/":
+    print(num1/num2)
